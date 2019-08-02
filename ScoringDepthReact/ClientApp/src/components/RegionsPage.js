0cc0a124e6c6e84f4b1a7592887ce115c1b43e39
ctoewsdev <caseytoews@telus.net>
2019-08-02 12:35:57 -0700

    Added function getSeasonsByYear() to filter state.seasons and return only the seasons matching myProps.yearId.

--- ScoringDepthReact/ClientApp/src/components/RegionsPage.js
+++ ScoringDepthReact/ClientApp/src/components/RegionsPage.js
@@ -10,6 +10,10 @@ import RegionsList from './RegionsList';
 
 class RegionsPage extends React.Component {
 
+    //state = {
+    //    seasonsList: []
+    //}
+
     //seasonName = {};
     //seasonRegionsList = [];
     //year = {};
@@ -49,6 +53,12 @@ class RegionsPage extends React.Component {
             });
         }
 
+        //if (seasons.length === 0) {
+        //    actions.loadSeasonsByYear().catch(error => {
+        //        alert("Loading seasons by year failed" + error);
+        //    });
+        //}
+
 
         if (regions.length === 0) {
             actions.loadRegions().catch(error => {
@@ -63,42 +73,32 @@ class RegionsPage extends React.Component {
         //}
 
 
-
+        //if (seasonsList.length === 0) {
+        //    seasonsList = [];
+        //}
 
 
     }
 
     //setSeasonName(name) {
-        //this.setSeasonName(seasonName);
-        //  this.setState({ seasonName: name })
-        // this.setState({ [state.seasonName]: state.year.name });
+    //this.setSeasonName(seasonName);
+    //  this.setState({ seasonName: name })
+    // this.setState({ [state.seasonName]: state.year.name });
     //}
 
     //setYearId(yearId) {
-        //this.setSeasonName(seasonName);
-       // this.setState({ yearId: yearId })
-        // this.setState({ [state.seasonName]: state.year.name });
-   // }
+    //this.setSeasonName(seasonName);
+    // this.setState({ yearId: yearId })
+    // this.setState({ [state.seasonName]: state.year.name });
+    // }
 
-
-
-
-    filterSeasons () {
-       const allSeasons = this.state.seasons;
-       const yearId = this.state.yearId;
-        const filtered = allSeasons.filter(seasons => seasons.yearId === yearId) || null;
-        
-        this.setState({
-            seasons: filtered
-        })
-    }
 
     render() {
         return (
             <>
                 <h1 class="text-center">Season:  </h1>
                 <h2 class="text-center">Please select a region</h2>
-                <RegionsList seasons={this.props.seasons} onChange={this.filterSeasons}/>
+                <RegionsList seasons={this.props.seasons} />
             </>
         );
     }
@@ -108,44 +108,65 @@ RegionsPage.propTypes = {
     years: PropTypes.array.isRequired,
     regions: PropTypes.array.isRequired,
     seasons: PropTypes.array.isRequired,
+    seasonsList: PropTypes.array.isRequired,
     actions: PropTypes.object.isRequired
 };
 
 
-export function getSeasonsByYearId(seasons, yearId) {
-    return seasons.filter(seasons => seasons.yearId === yearId) || null;
+export function getSeasonsByYear(seasons, yearId) {
+    return seasons.filter(season => season.yearId == yearId);
+ //   console.log('season by year array 2 is: ', sList);
+     //return sList; 
 }
 
 
 function mapStateToProps(state, ownProps) {
 
     const yearId = ownProps.match.params.yearId;
-    console.log(yearId);
+
+    var seasonsList = state.seasons.length > 0 ? getSeasonsByYear(state.seasons, yearId) : [];
 
 
-    const seasonList = getSeasonsByYearId(state.seasons, yearId);
-    console.log(seasonList);
+
+    //state.seasons.filter(season => season.yearId === yearId);
+    //console.log('seasonsList array is: ', seasonsList);
 
     //var year = getSeasonName(state.years, yearId);
     //setSeasonName(year.name);
 
-    //const seasons = yearId && state.seasons.length > 0 ? getSeasonsByYearId(state.seasons, yearId) : null;
-    //const seasonRegionsList = state.seasons.length > 0 && state.regions.length > 0 ? getSeasonRegionsList(state.regions, seasons, this.region, this.SeasonRegionsList) : null;
-
     return {
-        seasons: state.years.length === 0 || state.regions.length === 0
-            ? []
-            : state.seasons.map(season => {
-            return {
-                ...season,
-                yearName: state.years.find(y => y.yearId === season.yearId).name,
-                regionName: state.regions.find(r => r.regionId === season.regionId).name
-            };
-        }),
         yearId: yearId,
+        //seasonsList: state.seasons.length === 0 ? [] : state.seasons.filter(season => season.yearId === yearId),
+      //  seasonsList: seasonsList,
+        seasons: seasonsList.length === 0 || state.years.length === 0 || state.regions.length === 0
+            ? []
+            : seasonsList.map(season => {
+                return {
+                    ...season,
+                    yearName: state.years.find(y => y.yearId === season.yearId).name,
+                    regionName: state.regions.find(r => r.regionId === season.regionId).name
+                };
+            }),
+
         years: state.years,
         regions: state.regions
     };
+
+
+    //return {
+    //    seasons: state.years.length === 0 || state.regions.length === 0
+    //        ? []
+    //        : seasonsList.map(season => {
+    //            return {
+    //                ...season,
+    //                yearName: state.years.find(y => y.yearId === season.yearId).name,
+    //                regionName: state.regions.find(r => r.regionId === season.regionId).name
+    //            };
+    //        }),
+    //    yearId: yearId,
+    //    years: state.years,
+    //    regions: state.regions
+    //};
 }
 
 function mapDispatchToProps(dispatch) {
