766d6516b82a383d6201b2c0b62a4263bd90ef6b
ctoewsdev <caseytoews@telus.net>
2019-08-04 13:42:25 -0700

    Clean up Reducers and Initial State.

--- ScoringDepthReact/ClientApp/src/redux/reducers/feedbackReducer.js
+++ ScoringDepthReact/ClientApp/src/redux/reducers/feedbackReducer.js
@@ -2,11 +2,10 @@ import * as types from "../actions/actionTypes";
 import initialState from "./initialState";
 
 export default function feedbackReducer(state = initialState.feedback, action) {
-  switch (action.type) {
-      case types.CREATE_FEEDBACK_SUCCESS:
-          //I dont need to use an array here because I am just submitting one object
-      return [...state, { ...action.feedback }];
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case types.CREATE_FEEDBACK_SUCCESS:
+            return [...state, { ...action.feedback }];
+        default:
+            return state;
+    }
 }
--- ScoringDepthReact/ClientApp/src/redux/reducers/regionReducer.js
+++ ScoringDepthReact/ClientApp/src/redux/reducers/regionReducer.js
@@ -1,7 +1,6 @@
 import * as types from "../actions/actionTypes";
 import initialState from "./initialState";
 
-//whatever is returned from the reducer becomes the new state
 export default function regionReducer(state = initialState.regions, action) {
     switch (action.type) {
         case types.LOAD_REGIONS_SUCCESS:
