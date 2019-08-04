835bc4f18c6b9cef4f6cd7c42c91dbd31783cb97
ctoewsdev <caseytoews@telus.net>
2019-08-04 13:39:53 -0700

    Add redux dev tool reduxImmutableStateInvariant.

--- ScoringDepthReact/ClientApp/src/redux/configureStore.js
+++ ScoringDepthReact/ClientApp/src/redux/configureStore.js
@@ -3,16 +3,14 @@ import rootReducer from "./reducers";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import thunk from "redux-thunk";
 
-
-
 export default function configureStore(initialState) {
     // add support for Redux dev tools
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
+    const composeEnhancers =
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-  return createStore(
-    rootReducer,
-    initialState,
-    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
-  );
+    return createStore(
+        rootReducer,
+        initialState,
+        composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
+    );
 }
